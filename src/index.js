3ae119424c1225dc646175f72ebd5b3ac9b1a890
rasyidarazak <rasyidarazaks@gmail.com>
2023-03-20 17:53:38 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./css/bootstrap-dark.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import "./css/style.css";
+import "./css/dark-mode.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Main from "./components/Main";
+
+ReactDOM.render(<Main />, document.getElementById("root"));
